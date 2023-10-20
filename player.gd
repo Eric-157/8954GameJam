678b3f52127d3bd8985a6ebf96b59203565e435c
Eric Edwards <59770361+Eric-157@users.noreply.github.com>
2023-10-20 00:07:15 -0400

    Imports, better levels, and script adjustments
    Imported the art and music, switched out the tilemaps, adjusted the scripts

--- player.gd
+++ player.gd
@@ -1,13 +1,16 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -320.0
+const SPEED = 170.0
+const JUMP_VELOCITY = -520.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var anim = get_node("AnimatedSprite2D")
 
-
+func _ready():
+		anim.play("idle")
+		
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -19,10 +22,12 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
+		anim.play("walking")
 	else:
+		anim.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
